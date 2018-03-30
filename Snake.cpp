6798b469a302a969016df4b16c535489afdd90e8
Rajesh saha <33521669+rajeshsaha782@users.noreply.github.com>
2018-03-30 14:31:08 +0600

    Basic game done

--- Snake.cpp
+++ Snake.cpp
@@ -10,14 +10,10 @@ using namespace std;
 
 string direction="right";
 GLint  positionX=0,positionY=0,speed=10;
-int snake_size[10],Rx,Ry;
-bool visited=false;
+int Rx,Ry,snake_size=1,score=0;
+bool visited=false,status=false;
 
-void init()
-{
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glOrtho(0.0, 600.0, 0.0, 600.0, -1.0, 1.0);
-}
+
 
 int random(int min, int max) //range : [min, max)
 {
@@ -30,30 +26,51 @@ int random(int min, int max) //range : [min, max)
    return min + rand() % (( max + 1 ) - min);
 }
 
+void init()
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glOrtho(0.0, 600.0, 0.0, 600.0, -1.0, 1.0);
+    Rx=random(30,550);
+    Ry=random(30,550);
+    Rx=Rx-(Rx%10);
+    Ry=Ry-(Ry%10);
+}
+
 void RandomPoints()
 {
-     Rx=random(0,600);
-     Ry=random(0,600);
+     Rx=random(30,550);
+     Ry=random(30,550);
      Rx=Rx-(Rx%10);
      Ry=Ry-(Ry%10);
+     visited=false;
 }
 
 void RandomBox()
 {
     glBegin(GL_QUADS);
-    glColor3f(1.0f, 0.0f, 0.0f);
+    glColor3f(1.0f, 0.0f, 1.0f);
     glVertex2f(Rx,Ry);
     glVertex2f(Rx+10,Ry);
     glVertex2f(Rx+10,Ry+10);
     glVertex2f(Rx,Ry+10);
     glEnd();
-   // cout<<"X:"<<Rx<<"Y:"<<Ry<<endl;
+
+
+      if(positionX==Rx && positionY+10==Ry)
+        {
+           cout<<"X:"<<Rx<<" positionX:"<<positionX<<endl;
+           cout<<"Y:"<<Ry<<" positionY:"<<positionY<<endl;
+           visited=true;snake_size++;score++;
+           //speed=0;
+        }
+
+    //cout<<"X:"<<Rx<<"Y:"<<Ry<<endl;
 }
 
 void update(int value)
 {
 
-    if( positionX >= 700)
+    /*if( positionX >= 700)
          positionX = 0;
     else if( positionX <=-110)
          positionX = 600;
@@ -61,7 +78,7 @@ void update(int value)
     else if( positionY >= 700)
          positionY = 0;
     else if( positionY <= -110)
-         positionY = 600;
+         positionY = 600;*/
 
     if(direction=="right")
     {
@@ -115,57 +132,65 @@ void SpecialFunc(int key, int x, int y)
 
 }
 
-void Check()
+void Check_border()
 {
-    if(positionX>=590 || positionY>=590)
+    if(positionX>=590 || positionX<10|| positionY>=580 ||positionY<=-10)
     {
         speed =0;
+        if(!status)
+        {
+            cout<<"Check->"<<positionX<<","<<positionY<<endl;
+            cout<<"***********GAME OVER************"<<endl;
+            cout<<"**************Your Score:"<<score<<"**************"<<endl;
+            status=true;
+        }
+
+
     }
+
 }
 
 void Right(int a)
 {
-    //Check();
-
    glBegin(GL_QUADS);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex2f(0-a,100);
-    glVertex2f(10-a,100);
-    glVertex2f(10-a,110);
-    glVertex2f(0-a,110);
+
+    glVertex2f(0-a,10);
+    glVertex2f(10-a,10);
+    glVertex2f(10-a,20);
+    glVertex2f(0-a,20);
     glEnd();
-    cout<<"X:"<<10-a<<" Y:"<<100<<endl;
+    //cout<<"X:"<<10-a<<" Y:"<<100<<endl;
 }
 void Left(int a)
 {
    glBegin(GL_QUADS);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex2f(0+a,100);
-    glVertex2f(10+a,100);
-    glVertex2f(10+a,110);
-    glVertex2f(0+a,110);
+
+    glVertex2f(0+a,10);
+    glVertex2f(10+a,10);
+    glVertex2f(10+a,20);
+    glVertex2f(0+a,20);
     glEnd();
     //cout<<"X:"<<0<<"Y:"<<0+a<<endl;
 }
 void Top(int a)
 {
    glBegin(GL_QUADS);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex2f(0,100-a);
-    glVertex2f(10,100-a);
-    glVertex2f(10,110-a);
-    glVertex2f(0,110-a);
+
+    glVertex2f(0,10-a);
+    glVertex2f(10,10-a);
+    glVertex2f(10,20-a);
+    glVertex2f(0,20-a);
     glEnd();
     //cout<<"X:"<<0<<"Y:"<<0+a<<endl;
 }
 void Bottom(int a)
 {
    glBegin(GL_QUADS);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex2f(0,100+a);
-    glVertex2f(10,100+a);
-    glVertex2f(10,110+a);
-    glVertex2f(0,110+a);
+
+    glVertex2f(0,10+a);
+    glVertex2f(10,10+a);
+    glVertex2f(10,20+a);
+    glVertex2f(0,20+a);
     glEnd();
 
     //cout<<"X:"<<0<<"Y:"<<0+a<<endl;
@@ -173,10 +198,9 @@ void Bottom(int a)
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    if(!visited)
+    if(visited)
     {
       RandomPoints();
-      visited=true;
     }
     RandomBox();
 
@@ -193,8 +217,21 @@ void display()
     glEnd();
 
     int a=0;
-    for(int i=0;i<=10;i++)
+    for(int i=0;i<snake_size;i++)
     {
+        if(i==0)
+        {
+            Check_border();
+
+        }
+        if(i%2==0)
+        {
+            glColor3f(1.0f, 1.0f, 0.0f);
+        }
+        else
+        {
+            glColor3f(1.0f, 0.0f, 0.0f);
+        }
 
        glPushMatrix();
         glTranslatef( positionX,positionY, 0.0f);
@@ -216,7 +253,7 @@ void display()
         }
 
         glPopMatrix();
-        a=a+12;
+        a=a+10;
     }
 
     glFlush();
@@ -226,7 +263,7 @@ int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitWindowSize(600, 600);
-    glutInitWindowPosition(50, 50);
+    glutInitWindowPosition(350, 50);
     glutCreateWindow("Snake");
     glutDisplayFunc(display);
     init();
